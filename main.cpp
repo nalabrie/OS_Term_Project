8aaf9e3cadc46ebf4f70b35ec72fd50db51a21fd
nalabrie <nalabrie@student.ysu.edu>
2020-02-20 17:31:03 -0500

    Replaced the dummy VDI with the real one now that writing is done being tested. The real VDI is not tracked with git because it is too large for GitHub.

--- main.cpp
+++ main.cpp
@@ -7,10 +7,14 @@ using namespace std;
 
 int main(int argc, char **argv) {
     vdi file = vdi(argv[1]);
+    int size = 64;
+    char buffer[size];
 
-    vdi::printBuffer(file.header, 0x200);
+    cout << 0x100000;
 
-    cout << 0x200;
+//    file.seek(0x2001BE);
+//    file.read(buffer, size);
+//    vdi::printBuffer(buffer, size);
 
     return 0;
 }
